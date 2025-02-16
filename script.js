19fb996cb8e974444bf9eac7626e16bc121ea488
clone31415 <156554592+clone31415@users.noreply.github.com>
2025-02-16 20:32:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ class Planets{
 		this.tail = 30
 
 		this.ctx = ctx
-		this.integrator = integrator
+		this.integrator = integrator.bind(this)
 		this.planets = []
 	}
 
@@ -56,7 +56,6 @@ class Planets{
 			pos: new Vector2D(x, y), // m
 			vel: new Vector2D(vx, vy), // ms^-1
 			acc: new Vector2D(ax, ay), // ms^-2
-			prevAcc: new Vector2D(), // for velocity verlet
 			size, // pixel
 			prev: []
 		})
@@ -106,37 +105,6 @@ class Planets{
 		requestAnimationFrame(this.move.bind(this))
 	}
 
-	static euler(){
-		this.setAcc()
-
-		for (const planet of this.planets){
-			planet.vel = planet.vel.add(planet.acc.scale(this.dt / this.split))
-			planet.pos = planet.pos.add(planet.vel.scale(this.dt / this.split))
-		}
-	}
-
-	static velVerlet(){
-		for (const planet of this.planets) planet.pos = planet.pos.add(planet.vel.scale(this.dt / this.split)).add(planet.acc.scale((this.dt / this.split) ** 2 / 2))
-
-		this.setAcc()
-
-		for (const planet of this.planets){
-			planet.vel = planet.vel.add(planet.prevAcc.add(planet.acc).scale(this.dt / this.split / 2))
-			planet.prevAcc = planet.acc
-		}
-	}
-
-	static leapfrog(){
-		for (const planet of this.planets) planet.pos = planet.pos.add(planet.vel.scale(this.dt / this.split / 2))
-
-		this.setAcc()
-
-		for (const planet of this.planets){
-			planet.vel = planet.vel.add(planet.acc.scale(this.dt / this.split))
-			planet.pos = planet.pos.add(planet.vel.scale(this.dt / this.split / 2))
-		}
-	}
-
 	get energy(){
 		let energy = this.planets[0].mass * this.planets[0].vel.magSq / 2
 		
@@ -148,10 +116,97 @@ class Planets{
 
 		return energy
 	}
+
+	static euler(){
+		this.setAcc()
+
+		for (const planet of this.planets){
+			planet.vel = planet.vel.add(planet.acc.scale(this.dt / this.split))
+			planet.pos = planet.pos.add(planet.vel.scale(this.dt / this.split))
+		}
+	}
+
+	static leapfrog(){ // same as velocity verlet
+		for (const planet of this.planets) planet.pos = planet.pos.add(planet.vel.scale(this.dt / this.split / 2))
+
+		this.setAcc()
+
+		for (const planet of this.planets){
+			planet.vel = planet.vel.add(planet.acc.scale(this.dt / this.split))
+			planet.pos = planet.pos.add(planet.vel.scale(this.dt / this.split / 2))
+		}
+	}
+
+	static get yoshida4(){
+		// const cbrt2 = Math.cbrt(2), w1 = 1 / (2 - cbrt2), w0 = -cbrt2 * w1, coeff = {c: [w1 / 2, (w0 + w1) / 2, (w0 + w1) / 2, w1 / 2], d: [w1, w0, w1]}
+
+		// function symplectic_integrator_numeric(order) {
+		// 	if (order % 2 !== 0) {
+		// 		console.log("This integrator is only for even order");
+		// 		return;
+		// 	}
+		
+		// 	let S = [0.5, 1, 0.5];
+		
+		// 	if (order > 2) {
+		// 		for (let n = 1; n < order / 2; n++) {
+		// 			let alpha = Math.pow(2.0, (1.0 / (2.0 * n + 1)));
+		// 			let x0 = -alpha / (2.0 - alpha);
+		// 			let x1 = 1.0 / (2.0 - alpha);
+		// 			console.log(x0,x1)
+		
+		// 			let TC = S.map(i => i * x0);
+		// 			let TL = S.map(i => i * x1);
+		
+		// 			let T = [];
+		
+		// 			// Loop for TL[ : -1 ]
+		// 			for (let i = 0; i < TL.length - 1; i++) {
+		// 				T.push(TL[i]);
+		// 			}
+		
+		// 			// Add the last part of T
+		// 			T.push(TL[TL.length - 1] + TC[0]);
+		
+		// 			// Loop for TC[1: -1]
+		// 			for (let i = 1; i < TC.length - 1; i++) {
+		// 				T.push(TC[i]);
+		// 			}
+		
+		// 			// Add the first part of T
+		// 			T.push(TC[TC.length - 1] + TL[0]);
+		
+		// 			// Loop for TL[1:]
+		// 			for (let i = 1; i < TL.length; i++) {
+		// 				T.push(TL[i]);
+		// 			}
+		
+		// 			S = T;
+		// 		}
+		// 	}
+		
+		// 	return S;
+		// }
+
+		const coeff = {c: [0.7936124638611216, -0.20627658481643976, -0.20627658481643976, -0.11800886788129283, 0.20627658481643976, 0.20627658481643976, -0.11800886788129283, -0.20627658481643976, -0.20627658481643976, 0.7936124638611216], d: [1.5872249277222432, -1.999778097355123, 1.5872249277222432, -1.8232426634848289, 2.2971418107909303, -1.8232426634848289, 1.5872249277222432, -1.999778097355123, 1.5872249277222432]}
+		
+		return function(){
+			for (const planet of this.planets) planet.pos = planet.pos.add(planet.vel.scale(coeff.c[0] * this.dt / this.split))
+
+			for (let i = 0; i < coeff.d.length; i++){
+				this.setAcc()
+	
+				for (const planet of this.planets){
+					planet.vel = planet.vel.add(planet.acc.scale(coeff.d[i] * this.dt / this.split))
+					planet.pos = planet.pos.add(planet.vel.scale(coeff.c[i + 1] * this.dt / this.split))
+				}
+			}
+		}
+	}
 }
 
 
-const planets = new Planets(ctx, Planets.leapfrog) 
+const planets = new Planets(ctx, Planets.yoshida4)
 	
 planets.add(1, 400, 450, 0, 30, 0, 0, 3)
 planets.add(3e15, 800, 400, -30, 0, 0, 0, 10)
